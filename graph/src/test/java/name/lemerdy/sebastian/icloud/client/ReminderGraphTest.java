ae08d20dcac2cb7b403a8b922bfa2a5deefe78bc
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-06-03 09:17:34 +0200

    should fail if reminder in not found

--- graph/src/test/java/name/lemerdy/sebastian/icloud/client/ReminderGraphTest.java
+++ graph/src/test/java/name/lemerdy/sebastian/icloud/client/ReminderGraphTest.java
@@ -1,14 +1,31 @@
 package name.lemerdy.sebastian.icloud.client;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReminderGraphTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
     public void should_get_reminder_by_guid() {
-        Reminder reminder = new ReminderGraph("/reminders.json").getByGuid("1A6CE4B1-98ED-459F-ABA3-3301B43D9EC1");
+        ReminderGraph reminderGraph = new ReminderGraph("/reminders.json");
+
+        Reminder reminder = reminderGraph.getByGuid("1A6CE4B1-98ED-459F-ABA3-3301B43D9EC1");
 
         assertThat(reminder.guid).isEqualTo("1A6CE4B1-98ED-459F-ABA3-3301B43D9EC1");
+    }
+
+    @Test
+    public void should_fail_if_reminder_is_not_found() {
+        ReminderGraph reminderGraph = new ReminderGraph("/reminders.json");
+        thrown.expect(NoSuchElementException.class);
+
+        reminderGraph.getByGuid("N01F0UN0-GU10-0000-0000-000000000000");
     }
 }
