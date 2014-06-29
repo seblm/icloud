b1430c2f6124f59812c8e6b901efdcd072f1b76b
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-06-29 15:00:18 +0200

    resource for graphing reminders

--- graph/src/main/java/name/lemerdy/sebastian/icloud/client/Reminders.java
+++ graph/src/main/java/name/lemerdy/sebastian/icloud/client/Reminders.java
@@ -1,13 +1,36 @@
 package name.lemerdy.sebastian.icloud.client;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.NoSuchElementException;
+
+import static java.util.Arrays.asList;
 
 public class Reminders {
     public final List<Reminder> reminders;
 
-    public Reminders(@JsonProperty("Reminders") List<Reminder> reminders) {
-        this.reminders = reminders;
+    public Reminders(@JsonProperty("Reminders") Reminder... reminders) {
+        this.reminders = asList(reminders);
+    }
+
+    public Reminder getByGuid(String guid) {
+        return reminders.stream()
+                .filter(reminder -> guid.equals(reminder.guid))
+                .findFirst()
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+    public static Reminders readFromFile(String resourceName) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        try {
+            return mapper.readValue(Reminders.class.getResourceAsStream(resourceName), Reminders.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- graph/src/test/java/name/lemerdy/sebastian/icloud/client/RemindersTest.java
+++ graph/src/test/java/name/lemerdy/sebastian/icloud/client/RemindersTest.java
@@ -10,20 +10,20 @@ import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ReminderGraphTest {
+public class RemindersTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
-    private ReminderGraph reminderGraph;
+    private Reminders reminders;
 
     @Before
     public void createReminderGraph() throws Exception {
-        reminderGraph = new ReminderGraph("/reminders.json");
+        reminders = Reminders.readFromFile("/reminders.json");
     }
 
     @Test
     public void should_get_reminder_by_guid() {
-        Reminder reminder = reminderGraph.getByGuid("1A6CE4B1-98ED-459F-ABA3-3301B43D9EC1");
+        Reminder reminder = reminders.getByGuid("1A6CE4B1-98ED-459F-ABA3-3301B43D9EC1");
 
         assertThat(reminder.guid).isEqualTo("1A6CE4B1-98ED-459F-ABA3-3301B43D9EC1");
     }
@@ -32,26 +32,26 @@ public class ReminderGraphTest {
     public void should_fail_if_reminder_is_not_found() {
         thrown.expect(NoSuchElementException.class);
 
-        reminderGraph.getByGuid("N01F0UN0-GU10-0000-0000-000000000000");
+        reminders.getByGuid("N01F0UN0-GU10-0000-0000-000000000000");
     }
 
     @Test
     public void should_read_created_date() {
-        Reminder reminder = reminderGraph.getByGuid("1A6CE4B1-98ED-459F-ABA3-3301B43D9EC1");
+        Reminder reminder = reminders.getByGuid("1A6CE4B1-98ED-459F-ABA3-3301B43D9EC1");
 
         assertThat(reminder.createdDate.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)).isEqualTo("2014-05-19T04:45:00");
     }
 
     @Test
     public void should_read_completed_date() {
-        Reminder reminder = reminderGraph.getByGuid("1A6CE4B1-98ED-459F-ABA3-3301B43D9EC1");
+        Reminder reminder = reminders.getByGuid("1A6CE4B1-98ED-459F-ABA3-3301B43D9EC1");
 
-        assertThat(reminder.completedDate.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)).isEqualTo("2014-05-21T04:45:00");
+        assertThat(reminder.completedDate.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)).isEqualTo("2014-05-21T16:23:00");
     }
 
     @Test
     public void should_read_title() {
-        Reminder reminder = reminderGraph.getByGuid("1A6CE4B1-98ED-459F-ABA3-3301B43D9EC1");
+        Reminder reminder = reminders.getByGuid("1A6CE4B1-98ED-459F-ABA3-3301B43D9EC1");
 
         assertThat(reminder.title).isEqualTo("Lampes de chevet");
     }
